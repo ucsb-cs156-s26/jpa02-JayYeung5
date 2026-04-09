28345be9c60f59406ac09c6822a90a6018f4ae3f
Jay <jayyeung@ucsb.edu>
2026-04-09 15:15:50 -0700

    xy - updated team members

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -34,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-12");
+        team.addMember("Arjun M.");
+        team.addMember("Arjun S.");
+        team.addMember("Jay");
+        team.addMember("Lorenzo");
+        team.addMember("Pau");
+        team.addMember("Pengyu");
         return team;
     }
 }
