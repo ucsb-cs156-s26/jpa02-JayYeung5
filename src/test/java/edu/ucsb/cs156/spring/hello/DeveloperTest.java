37d07e40a802461e3f59579ae00da2bee332c4c1
Jay <jayyeung@ucsb.edu>
2026-04-09 15:10:06 -0700

    xy - updated with actual githubid

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Jay", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("JayYeung5", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
